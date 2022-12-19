27ca1c3e2520208bf0c36906d36c23720c3f7301
FIRST_NAME LAST_NAME <rb.firez@gmail.com>
2022-12-19 22:40:35 +0530

    in

---
+++ script.js
@@ -0,0 +1,16 @@
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+setInterval(function () {
+  let curHour = new Date().getHours();
+
+  curHour > 12 ? (curHour = curHour - 12) : curHour;
+  const newHour = curHour + new Date().getMinutes() / 60;
+
+  hour.style.rotate = `${newHour * 30 - 90}deg`;
+  min.style.rotate = `${
+    (new Date().getMinutes() + new Date().getSeconds() / 60) * 6 - 93
+  }deg`;
+  sec.style.rotate = `${new Date().getSeconds() * 6 - 105}deg`;
+}, 500);
